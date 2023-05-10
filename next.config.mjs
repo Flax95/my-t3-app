8dcc0616897a6b3d9f82cba62c6c024820392acf
Soren Madsen <soren.madsen@prevas.se>
2023-05-10 13:56:50 +0200

    Revert "Adds next-pwa"
    This reverts commit 6041fd5e39299aa8e61c10144db402e83acf2d12.

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,10 @@
  * for Docker builds.
  */
 await import("./src/env.mjs");
-import nextPWA from "next-pwa";
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-  swcMinify: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV !== "development",
-  },
 
   /**
    * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
@@ -27,15 +22,4 @@ const config = {
     domains: ["images.clerk.dev"],
   },
 };
-
-console.log(nextPWA);
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = nextPWA({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
-
-export default withPWA(config);
+export default config;
